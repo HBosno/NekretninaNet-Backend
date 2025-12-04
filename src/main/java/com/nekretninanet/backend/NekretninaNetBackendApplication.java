98cfa0591d4e80e0bc3df8568b557d33baf30fb7
HBosno <hbosno1@etf.unsa.ba>
2025-12-04 11:44:56 +0100

    initial commit

---
+++ src/main/java/com/nekretninanet/backend/NekretninaNetBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nekretninanet.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NekretninaNetBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NekretninaNetBackendApplication.class, args);
+	}
+
+}
